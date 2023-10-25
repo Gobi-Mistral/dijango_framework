ad3c3bce42e947f7af236b4bc6fa595c997cf34e
Gobi <gobi.e@mistralsolutions.com>
2023-10-25 16:25:21 +0530

    1. env - updated DESKTOP_APP path 2. Settings - Added jenkins support 3. Requirements - Added supported library 4. Setup.bat - Added update the env file method and test execution methods

--- Setup.bat
+++ Setup.bat
@@ -64,7 +64,7 @@ EXIT /B 0
 	EXIT /B %ERRORLEVEL%
 	
 :run_test
-	python manage.py test %~1
+	python manage.py jenkins %~1
 	if %ERRORLEVEL% EQU 0 (
 		echo "****** %~1 (%cnt%) - PASS"
 	) else (
@@ -73,10 +73,15 @@ EXIT /B 0
 	EXIT /B %ERRORLEVEL%	
 	
 :backup_report
-	set report_name=Report_%~1	
+	set report_name=Report_%~1
 
 	copy reports\junit.xml reports\%report_name%.xml
 	
+	EXIT /B %ERRORLEVEL%
+	
+:updated_env_file
+	sed -i "s|^%~1=.*|%~1=%~2|" .env
+	timeout /t 3 /nobreak
 	EXIT /B %ERRORLEVEL%
 	
 :Web_Browser_Test
@@ -92,7 +97,94 @@ EXIT /B 0
 	EXIT /B %ERRORLEVEL%
 	
 :Desktop_Apps_Test
-	echo Android Test - Testing Desktop Apps
+	echo Desktop Test - Testing Desktop Apps
 	CALL :run_test dijango_framework.test_modules.test_desktop_app.TestDesktop.test_003_interface_test_server
 	CALL :backup_report Desktop_Apps
+	EXIT /B %ERRORLEVEL%
+	
+:Different_Web_Browser_Test
+	echo ==============================================================================
+	echo Chrome Browser Test - Testing Asus Login with Chrome Browser
+	echo ==============================================================================
+	CALL :updated_env_file PLATFORM WEB
+	CALL :updated_env_file BROWSER CHROME
+	CALL :run_test dijango_framework.test_modules.test_web_apps
+	CALL :backup_report Chrome_Browser_Asus_Router
+	
+	echo ==============================================================================
+	echo Firefox Browser Test - Testing Asus Login with Firefox Browser
+	echo ==============================================================================
+	CALL :updated_env_file PLATFORM WEB
+	CALL :updated_env_file BROWSER FIREFOX
+	CALL :run_test dijango_framework.test_modules.test_web_apps
+	CALL :backup_report Firefox_Browser_Asus_Router
+	
+	echo ==============================================================================
+	echo Edge Browser Test - Testing Asus Login with Edge Browser
+	echo ==============================================================================
+	CALL :updated_env_file PLATFORM WEB
+	CALL :updated_env_file BROWSER EDGE
+	CALL :run_test dijango_framework.test_modules.test_web_apps
+	CALL :backup_report EDGE_Browser_Asus_Router
+	
+	EXIT /B %ERRORLEVEL%
+	
+:Desktop_Different_Apps_Test
+	echo ==============================================================================
+	echo Desktop Test 1 - Testing Windows10 Calculator App
+	echo ==============================================================================
+	CALL :updated_env_file PLATFORM DESKTOP
+	CALL :updated_env_file DESKTOP_APP Microsoft.WindowsCalculator_8wekyb3d8bbwe!App
+	rem sed -i 's/^DESKTOP_APP=.*/DESKTOP_APP=Microsoft.WindowsCalculator_8wekyb3d8bbwe!App/' .env
+	rem timeout /t 3 /nobreak
+	CALL :run_test dijango_framework.test_modules.test_desktop_app.TestDesktop.test_001_calculator_app
+	CALL :backup_report Desktop_App_Calculator
+	
+	echo ==============================================================================
+	echo Desktop Test 2 - Testing Activation Key Generator App
+	echo ==============================================================================
+	CALL :updated_env_file DESKTOP_APP D://Software_Installation//ScripTalk//Activation_Key_Generator.exe
+	CALL :run_test dijango_framework.test_modules.test_desktop_app.TestDesktop.test_002_activation_key_generator
+	CALL :backup_report Desktop_App_Activation_Key_Generator
+	
+	echo ==============================================================================
+	echo Desktop Test 3 - Testing Test Data Server App
+	echo ==============================================================================
+	CALL :updated_env_file DESKTOP_APP D://Software_Installation//ScripTalk//InterfaceTestServer.exe
+	CALL :run_test dijango_framework.test_modules.test_desktop_app.TestDesktop.test_003_interface_test_server
+	CALL :backup_report Desktop_App_Test_Data_Server
+	
+	echo ==============================================================================
+	echo Desktop Test 4 - Testing Windows10 Groove Music App
+	echo ==============================================================================
+	CALL :updated_env_file DESKTOP_APP Microsoft.ZuneMusic_8wekyb3d8bbwe!Microsoft.ZuneMusic
+	CALL :run_test dijango_framework.test_modules.test_desktop_app.TestDesktop.test_004_desktop_groove_music_app
+	CALL :backup_report Desktop_App_Groove_Music
+	
+	EXIT /B %ERRORLEVEL%
+
+:repeat_run_test
+	SET cnt=1
+	SET TEST_COUNT=3
+
+	echo ==============================================================================
+	echo Repeat Test Iteration Start - %cnt% End - %TEST_COUNT%	
+	echo ==============================================================================
+	REM SET /P "RUN_TEST="
+
+	:continue_loop
+	if %cnt% leq %TEST_COUNT% (
+		echo "*** TEST ITERATION (%cnt%) ***"	
+		
+		CALL :Different_Web_Browser_Test
+		CALL :Desktop_Different_Apps_Test
+
+		ping 127.0.0.1 -n 2 > nul
+
+		SET /A "cnt = cnt + 1"
+
+		goto :continue_loop
+	)
+	SET cnt=1
+	SET TEST_COUNT=1
 	EXIT /B %ERRORLEVEL%
